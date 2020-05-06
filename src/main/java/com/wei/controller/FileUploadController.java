c30750233ee0d8ecb3f3af3179d8926fe62403cb
zeitlmz <2249297157@qq.com>
2020-05-06 21:16:15 +0800

    实现评论功能二级评论平铺，而不是无限嵌套

--- src/main/java/com/wei/controller/FileUploadController.java
+++ src/main/java/com/wei/controller/FileUploadController.java
@@ -1,13 +1,13 @@
 package com.wei.controller;
 
 import com.baomidou.mybatisplus.extension.api.ApiController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,12 +16,31 @@ import java.io.IOException;
  */
 @CrossOrigin
 @RestController
-@RequestMapping("image")
 public class FileUploadController extends ApiController {
-    @Value("${upload.picture.path}")
-    private String uploadPicturePath;
+//    @Value("${upload.picture.path}")
+
+    /**
+     * 图片访问路径
+     */
+    private String accUrl = "";
     @javax.annotation.Resource
     private ResourceLoader resourceLoader;
+
+
+    public static File getImgDirFile() {
+
+        // 构建上传文件的存放 "文件夹" 路径
+        /**
+         * 上传路径
+         */
+        String fileDirPath = new String("src/main/resources/templates/userData/img/");
+        File fileDir = new File(fileDirPath);
+        if (!fileDir.exists()) {
+            // 递归生成文件夹
+            fileDir.mkdirs();
+        }
+        return fileDir;
+    }
 
     /**
      * 上传
@@ -30,20 +49,29 @@ public class FileUploadController extends ApiController {
      * @author oneTi
      * @date 15:10 2018/8/17
      **/
-    @RequestMapping("/upload")
+    @RequestMapping("upload")
     @ResponseBody
-    public String upload(@RequestParam("file") MultipartFile multipartFile) {
+    public String upload(@RequestParam("editormd-image-file") MultipartFile multipartFile) {
+        String fileName = "";
+        String path = getImgDirFile().getAbsolutePath();
         try {
             //multipartFile.getOriginalFilename() 获取文件原始名称
+            fileName = multipartFile.getOriginalFilename();
             //new File(multipartFile.getOriginalFilename()) 根据获取到的原始文件名创建目标文件
             //multipartFile.transferTo() 将收到的文件传输到目标文件中
-            //multipartFile.transferTo(new File(multipartFile.getOriginalFilename()));
-            multipartFile.transferTo(new File(multipartFile.getOriginalFilename()));
+            assert fileName != null;
+            multipartFile.transferTo(new File(path + fileName));
+//            String accUrl="http://localhost:8081/"+path + fileName;
+            String accUrl = path + fileName;
+
+            System.out.println(accUrl);
+            this.accUrl = path;
+            return accUrl;
         } catch (IOException e) {
             e.printStackTrace();
-            return "true";
+            System.out.println("图片上传失败！");
+            return "图片上传失败";
         }
-        return "false";
     }
 
     /**
@@ -55,12 +83,12 @@ public class FileUploadController extends ApiController {
      * @date 15:11 2018/8/17
      **/
     @RequestMapping("/{fileName:.+}")
-    @ResponseBody
     public ResponseEntity<Resource> show(@PathVariable String fileName) {
         try {
             //resourceLoader.getResource("file:" + uploadPicturePath + fileName) 返回指定路径的资源句柄，这里返回的就是URL [file:D:/springboot/upload/test.png]
             //ResponseEntity.ok(T) 返回指定内容主体
-            return ResponseEntity.ok(resourceLoader.getResource("file:" + uploadPicturePath + fileName));
+            System.out.println(accUrl);
+            return ResponseEntity.ok(resourceLoader.getResource("file:" + this.accUrl + fileName));
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
